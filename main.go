24123e4848e77168c077d51e05b30e5d1225b29e
jx11r <jx11r@hotmail.com>
2023-04-04 15:49:06 -0600

    refactor: remove redundant code

--- main.go
+++ main.go
@@ -12,8 +12,7 @@ func task(id string, fn func() error) {
 	for {
 		if err := fn(); err != nil {
 			fmt.Printf("%s: %s\n", id, err.Error())
-			time.Sleep(time.Minute * 2)
-			continue
+			time.Sleep(time.Minute)
 		}
 		time.Sleep(time.Minute)
 	}
