32cc91f9ba2310fef53c52493326f38716f50488
jx11r <jx11r@hotmail.com>
2023-04-04 15:19:08 -0600

    refactor: update main loop

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"time"
 
 	"github.com/jx11r/qnotifier/provider/github"
@@ -23,11 +22,5 @@ func task(id string, fn func() error) {
 func main() {
 	go task("issues", github.Issues)
 	go task("releases", github.Releases)
-	go task("reddit", reddit.Posts)
-
-	fmt.Println("Press Enter to exit...")
-	if _, err := fmt.Scanln(); err != nil {
-		fmt.Println("Error reading input:", err)
-		os.Exit(1)
-	}
+	task("reddit", reddit.Posts)
 }
